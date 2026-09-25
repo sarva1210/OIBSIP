643563362e62c27e9486e2eb99d2f7f442e33aa7
sarva1210 <sarva.vish77@gmail.com>
2026-09-25 11:13:09 +0530

    elements

---
+++ WebDev-L2-ToDoApp/script.js
@@ -0,0 +1,13 @@
+const taskForm = document.getElementById("taskForm");
+const taskInput = document.getElementById("taskInput");
+const addTaskButton = document.getElementById("addTaskButton");
+const formMessage = document.getElementById("formMessage");
+
+const pendingTasks = document.getElementById("pendingTasks");
+const completedTasks = document.getElementById("completedTasks");
+
+const pendingCount = document.getElementById("pendingCount");
+const completedCount = document.getElementById("completedCount");
+
+const pendingEmpty = document.getElementById("pendingEmpty");
+const completedEmpty = document.getElementById("completedEmpty");
